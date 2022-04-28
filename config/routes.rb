e0bad8f6e394ee395d2189856e3ac9055441b219
tf-jlemasters <jlemasters@tracefirst.com>
2022-04-28 16:21:33 -0600

    upgraded rails due to GitHub security breach

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_scope :user do get '/users/sign_out', to: 'devise/sessions#destroy'
   end
+  devise_scope :user do get '/users/sign_up', to: 'devise/registrations#new'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
